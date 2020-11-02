05d5d6ba03e51adc8048b452684d71bbaec17b8f
Guilherme Ferreira <guifs2@hotmail.com>
2020-11-01 22:18:17 -0300

    v0 inicial

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,3 +9,13 @@ function selectImage(event) {
     }
 }
 
+// Adiciona um evento quando o usuário der scroll
+window.addEventListener('scroll', function(e) {
+    //console.log(window.pageYOffset) // Em qual altura de página o usuário está
+    let menu = document.getElementById('menu') //Identifica o menu na página
+    if(window.pageYOffset > 0) { //Maior que 100 pois a div #home não está colada no topo, mas poderia ser > 0.
+        menu.classList.add('scrolled')  //Adiciona a classe "scrolled" no menu
+    } else {
+        menu.classList.remove('scrolled')   //Remove a classe "scrolled" no menu
+    }
+});
