adf67129fe4caaf711c98097547b87c0933cb6bc
Guilherme Ferreira <guifs2@hotmail.com>
2020-11-04 13:30:12 -0300

    ajuste no menu e competencias

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,9 +13,10 @@ function selectImage(event) {
 window.addEventListener('scroll', function(e) {
     //console.log(window.pageYOffset) // Em qual altura de página o usuário está
     let menu = document.getElementById('menu') //Identifica o menu na página
-    if(window.pageYOffset > 0) { //Maior que 100 pois a div #home não está colada no topo, mas poderia ser > 0.
+    if(window.pageYOffset > 10) { //Maior que 100 pois a div #home não está colada no topo, mas poderia ser > 0.
         menu.classList.add('scrolled')  //Adiciona a classe "scrolled" no menu
     } else {
         menu.classList.remove('scrolled')   //Remove a classe "scrolled" no menu
     }
 });
+
